57c5e8f4c57ed01774438c2d54d64579fcdb2051
Greg Kezos <greg.kezos@gmail.com>
2019-12-06 16:29:07 -0700

    step 6 done

--- server/controllers/treasureController.js
+++ server/controllers/treasureController.js
@@ -3,6 +3,13 @@ module.exports = {
     const db = req.app.set('db')
     const result = await db.get_dragon_treasure(1)
     return res.status(200).send(result)
+  },
+
+
+  getUserTreasure: async (req, res) => {
+    const db = req.app.set('db')
+    const result = await db.get_user_treasure(req.session.user.id)
+    res.status(200).send(result)
   }
 
 }
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const massive = require('massive')
 const gs = require('gradient-string')
 const authCtrl = require('./controllers/authController')
 const treasureCtrl = require('./controllers/treasureController')
+const auth = require('./middleware/authMiddleware')
 const app = express()
 
 app.use(express.json())
@@ -32,6 +33,7 @@ app.post('/auth/login', authCtrl.login)
 app.get('/auth/logout', authCtrl.logout)
 
 app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure)
 
 
 app.listen(SERVER_PORT, () => console.log(gs.pastel(`Server live on ${SERVER_PORT}`)))
