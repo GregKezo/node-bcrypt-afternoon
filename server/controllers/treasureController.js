a120a4857240aa8e7454d74b0c730240824dd02f
Greg Kezos <greg.kezos@gmail.com>
2019-12-07 11:48:12 -0700

    done.

--- server/controllers/treasureController.js
+++ server/controllers/treasureController.js
@@ -10,6 +10,23 @@ module.exports = {
     const db = req.app.set('db')
     const result = await db.get_user_treasure(req.session.user.id)
     res.status(200).send(result)
+  },
+
+  addUserTreasure: async (req, res) => {
+    const { treasureURL } = req.body
+    const { id } = req.session.user
+    const db = req.app.set('db')
+
+    let userTreasure = await db.add_user_treasure([treasureURL, id])
+
+    res.status(200).send(userTreasure)
+  },
+
+  getAllTreasure: async (req, res) => {
+    const db = req.app.set('db')
+    const allTreasure = await db.get_all_treasure();
+    return res.status(200).send(allTreasure)
+
   }
 
 }
--- server/index.js
+++ server/index.js
@@ -34,6 +34,8 @@ app.get('/auth/logout', authCtrl.logout)
 
 app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
 app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure)
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure)
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure);
 
 
 app.listen(SERVER_PORT, () => console.log(gs.pastel(`Server live on ${SERVER_PORT}`)))
